536b50c2e730bcd9ccf31bf78a817e6d08e11837
linhcandoit <vutuanlinh02122002@gmail.com>
2024-07-16 10:25:38 +0700

    commit

--- apps/server/src/crons/crawl/cron.ts
+++ apps/server/src/crons/crawl/cron.ts
@@ -3,7 +3,7 @@ import { crawlJob } from './job'
 
 const crawlCron = CronJob.from({
   cronTime: '*/10 * * * * *',
-  onTick: function () {},
+  onTick: crawlJob,
   start: false,
   timeZone: 'Asia/Ho_Chi_Minh',
 })
