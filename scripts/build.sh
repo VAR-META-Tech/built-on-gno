45e0715c7fadaf33ff4945aab042b0317cb55370
dat tran <dattq@var-meta.com>
2024-07-31 17:23:29 +0700

    add out/ directory

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,13 @@
 # !/bin/bash
 set -eu
 
-turbo prune server --docker
-
 ls -la
+
+cp -r out/json apps/$BUILD_DIR
+cp out/pnpm-lock.yaml apps/$BUILD_DIR
+cp turbo.json apps/$BUILD_DIR
+cp .gitignore apps/$BUILD_DIR
+cp -r out/full apps/$BUILD_DIR
 
 cd apps/$BUILD_DIR
 
