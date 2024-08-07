25443e6de38a7c3680d5d8ed3643410099fd2dd1
Hoan Le Huu <hoanle396@gmail.com>
2024-08-07 17:40:42 +0700

    update favicon

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,6 @@
 'use client'
+import { Footer, Header } from '@/layouts'
 import '@/styles/global.css'
-import { Header, Footer } from '@/layouts'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Karla } from 'next/font/google'
@@ -23,6 +23,15 @@ const RootLayout = ({
 }>) => {
   return (
     <html lang="en">
+      <head>
+        <title>Built On Gno</title>
+        <meta name="description" content="Built On Gno Ecosystem" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="application-name" content="BuiltOnGno" />
+        <meta name="apple-mobile-web-app-title" content="BuiltOnGno" />
+      </head>
       <body
         className={`${karla.variable} bg-primary dark relative min-h-screen overflow-x-hidden`}
       >
