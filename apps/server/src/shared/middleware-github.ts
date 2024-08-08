3edf359964bfba2797d7c61aa338b4dbaf3d4d45
Hoan Le Huu <hoanle396@gmail.com>
2024-08-08 10:47:24 +0700

    update middlewares github (#53)

--- apps/server/src/shared/middleware-github.ts
+++ apps/server/src/shared/middleware-github.ts
@@ -4,7 +4,7 @@ function authenticationGithub(req: Request, res: Response, next: NextFunction) {
   try {
     const token = (req.headers.authorization ?? '').split(' ')[1]
     if (token === process.env.AUTH_TOKEN) {
-      next()
+      return next()
     }
     return res.status(403).send({
       code: 403,
