8fd3dfe0a816a38ea183ebe9f1f94631a52af81d
Hoan Le Huu <hoanle396@gmail.com>
2024-08-12 08:50:03 +0700

    fix issue frontend

--- apps/web/src/components/HeroSection/index.tsx
+++ apps/web/src/components/HeroSection/index.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { Button, Github, Rectangle } from '@repo/ui'
 import { PlaneIcon } from '@var-meta/icons'
-import { Heading } from '@var-meta/ui'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -9,7 +8,7 @@ const HeroSection = () => {
   return (
     <div className="relative flex h-[100vh] min-h-96 w-full flex-col items-center justify-center">
       <div className="bg-primary absolute inset-0 h-full w-full bg-[linear-gradient(to_right,#80808020_2px,transparent_2px),linear-gradient(to_bottom,#80808020_2px,transparent_2px)] bg-[size:128px_128px]" />
-      <div className="absolute left-[calc(50%_-_160%_/_2)] top-0 z-10 h-[1080px] w-[160%] flex-none overflow-hidden bg-[radial-gradient(50%_50%_at_50%_0%,_rgba(0,_89,_255,_.48)_0%,_rgba(0,_89,_255,_0)_100%)]" />
+      <div className="absolute left-[calc(50%_-_100%_/_2)] top-0 z-10 h-[100vh] w-[100%] flex-none overflow-hidden bg-[radial-gradient(50%_50%_at_50%_0%,_rgba(0,_89,_255,_.48)_0%,_rgba(0,_89,_255,_0)_100%)]" />
       <div className="absolute bottom-[10%] left-[30%] hidden overflow-visible md:flex">
         <Image
           src="/gnoscan.png"
@@ -27,28 +26,55 @@ const HeroSection = () => {
           </div>
         </div>
         <div className="container relative z-10 flex w-full flex-col items-center justify-around gap-8">
-          <Heading
-            size="6xl"
-            weight="semibold"
-            className="text-white"
-            align="center"
-          >
-            Built on Gno
-          </Heading>
+          <p className="text-center text-6xl font-medium text-white">
+            <span className="text-white">Built on</span>
+            <span className="inline-flex w-fit items-center text-white">
+              <span className="text-white">&nbsp;Gn</span>
+              <Image
+                width={72}
+                height={72}
+                className="-mb-5"
+                alt=""
+                src="/gno.logo.svg"
+              />
+            </span>
+          </p>
           <p className="max-w-screen-md text-center text-lg text-white">
             Listed below are the top crypto coins and tokens used for the
-            Gno.land Ecosystem. They are listed in size by many contributors. We
-            welcome you to contribute your projects to the system, please create
-            a pull request to{' '}
+            <span className="inline-flex w-fit items-center text-white">
+              <span className="text-white">&nbsp;Gn</span>
+              <Image
+                width={20}
+                height={20}
+                className="-mb-1"
+                alt=""
+                src="/gno.logo.svg"
+              />
+              <span>.land&nbsp;</span>
+            </span>
+            Ecosystem. They are listed in size by many contributors. We welcome
+            you to contribute your projects to the system, please create a pull
+            request to{' '}
             <Link
-              href="https://github.com/VAR-META-Tech/built-on-gno"
+              href="https://github.com/VAR-META-Tech/built-on-gno/issues"
               target="_blank"
               className="inline-flex w-fit items-start gap-1 text-white"
             >
               <Github className="h-5 w-5" color="#0059ff" />{' '}
               <span className="text-lg text-[#0059ff]">github</span>
             </Link>{' '}
-            if you have one or many projects/tools built with gno.land.
+            if you have one or many projects/tools built with
+            <span className="inline-flex w-fit items-center text-white">
+              <span className="text-white">&nbsp;Gn</span>
+              <Image
+                width={20}
+                height={20}
+                className="-mb-1"
+                alt=""
+                src="/gno.logo.svg"
+              />
+              <span>.land.</span>
+            </span>
           </p>
 
           <Link href="/ecosystem/project/all">
