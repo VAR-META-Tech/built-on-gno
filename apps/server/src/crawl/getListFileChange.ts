9de6ed631e4c0edb366f100a10002b6db8533ecb
Huy Vo <huyvo@var-meta.com>
2024-09-18 10:33:02 +0700

    add project

--- apps/server/src/crawl/getListFileChange.ts
+++ apps/server/src/crawl/getListFileChange.ts
@@ -21,8 +21,8 @@ function listProjectChanges(output: string): Array<string> {
   if (projectsRaw.length == 0) return []
 
   const projects = projectsRaw.map((data) => {
-    const arr = data.split(' ')
-    return arr[1]
+    const arr = data.split('|')
+    return arr[0].trim()
   })
   return projects
 }
@@ -33,7 +33,7 @@ export function getListFileChange(): Promise<Array<string>> {
       const merge = getIDsMergeAction(stdout)
 
       exec(
-        `git diff --stat ${merge.past} ${merge.recent}`,
+        `git diff --name-only ${merge.past} ${merge.recent}`,
         (error, stdout, stderr) => {
           res(listProjectChanges(stdout))
         },
