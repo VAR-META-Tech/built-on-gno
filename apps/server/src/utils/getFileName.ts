8582ae87459c1bd05593ab32b55188bb7e92b87f
Huy <102900841+hiimdev@users.noreply.github.com>
2024-09-19 14:32:37 +0700

    feat: debug (#106)
    Co-authored-by: Huy Vo <huyvo@var-meta.com>

--- apps/server/src/utils/getFileName.ts
+++ apps/server/src/utils/getFileName.ts
@@ -1,8 +1,10 @@
-export function getFileName(path: string): string {
-  if (!path || path === '/') return '';
-
-  const sanitizedPath = path.endsWith('/') ? path.slice(0, -1) : path;
-
-  const array = sanitizedPath.split('/');
-  return array[array.length - 1] || '';
+export function getFileName(path: string) {
+  if (path == '/') return ''
+  if (path[path.length - 1] !== '/') {
+    const array = path.split('/')
+    return array[array.length - 1]
+  } else {
+    const array = path.split('/')
+    return array[array.length - 2]
+  }
 }
